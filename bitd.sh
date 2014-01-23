9e7483a26d8a97d54995296f63d96c540b8cca4a
Niall Ryan <ryann4@tcd.ie>
2014-01-23 20:24:00 +0000

    Expanded BITD

--- bitd.sh
+++ bitd.sh
@@ -61,6 +61,7 @@ then
 	echo "form.html exists!"
 else
 	echo "form.html is not present!"
+	ERRORCHECK=$((ERRORCHECK+1))
 fi
 
 ACCEPT_FORM="DeploymentWebApp/cgi-bin/accept_form.pl"
@@ -68,6 +69,7 @@ if [ -e "$ACCEPT_FORM" ]; then
 	echo "accept_form.pl exists!"
 else
 	echo "accept_form.pl is not present!"
+	ERRORCHECK=$((ERRORCHECK+1))
 fi
 
 HELLO="DeploymentWebApp/cgi-bin/hello_world.pl"
@@ -75,6 +77,7 @@ if [ -e "$HELLO" ]; then
 	echo "hello_world.pl exists!"
 else
 	echo "hello_world.pl is not present!"
+	ERRORCHECK=$((ERRORCHECK+1))
 fi
 
 TESTDB="DeploymentWebApp/cgi-bin/testdb.pl"
@@ -82,54 +85,206 @@ if [ -e "$TESTDB" ]; then
 	echo "testdb.pl exists!"
 else
 	echo "testdb.pl is not present!"
+	ERRORCHECK=$((ERRORCHECK+1))
 fi
 
 # integrate static html content from 2 or more files into 1
 cat DeploymentWebApp/www/content.html DeploymentWebApp/www/image.html > DeploymentWebApp/www/index.html
+INDEX="DeploymentWebApp/www/index.html"
+if [ -e "$INDEX" ]
+then
+	echo "index.html created successfully"
+else
+	echo "index.html not created successfully"
+	ERRORCHECK=$((ERRORCHECK+1))
+fi
 
-# tar it back up
-# tar -zcvf webpackage_preIntegrate.tgz DeploymentWebApp
+# clean environment (uninstall + reinstall apache + mysql)
+# stop services
+service apache2 stop
+service mysql stop
 
-# set ERRORCHECK if any errors
+# uninstall
+apt-get -q -y remove apache2
+apt-get -q -y remove mysql-client mysql-server
+echo mysql-server mysql-server/root_password password password | debconf-set-selections
+echo mysql-server mysql-server/root_password_again password password | debconf-set-selections
+
+# refresh apt package repo
+apt-get update
+
+# reinstall
+apt-get install apache2
+apt-get install mysql-client mysql-server
+
+# restart apache and mysql
+service apache2 start
+service mysql start
+
+# tar package back up
+tar -zcvf webpackage_preIntegrate.tgz DeploymentWebApp
 
 # # move webpackage to Integrate dir and clean up
-# mv webpackage_preIntegrate.tgz ../integrate
-# rm -rf DeploymentWebApp
+mv webpackage_preIntegrate.tgz ../integrate
+rm -rf DeploymentWebApp
 
-# # untar
-# cd ../integrate
-# tar -zxvf webpackage_preIntegrate.tgz
+# untar
+cd ../integrate
+tar -zxvf webpackage_preIntegrate.tgz
 
-# # perform integrate/manipulation functions
+# move html files to apache /www
+cd DeploymentWebApp
+cp www/* /var/www
 
-# # tar it back up
-# tar -zcvf webpackage_preTest.tgz DeploymentWebApp
+# move perl files to /cgi-bin
+cp cgi-bin/* /usr/lib/cgi-bin
 
-# # set ERRORCHECK if any errors
+# make perl files executable
+chmod a+x /usr/lib/cgi-bin/*
+
+# return to sandbox
+cd ..
+
+# check files were copied successfully
+IND="/var/www/index.html"
+FORM="/var/www/form.html"
+AFPL="/usr/lib/cgi-bin/accept_form.pl"
+HW="/usr/lib/cgi-bin/hello_world.pl"
+TDB="/usr/lib/cgi-bin/testdb.pl"
+
+if [ -e "$IND" ] && [ -e "$FORM" ] && [ -e "$AFPL" ] && [ -e "$HW" ] && [ -e "$TDB" ]
+then
+	echo "HTML and Perl files in place"
+else
+	echo "HTML and Perl files NOT in place"
+	ERRORCHECK=$((ERRORCHECK+1))
+fi
+
+# tar it back up
+tar -zcvf webpackage_preTest.tgz DeploymentWebApp
 
 # # move to test dir and clean up
-# mv webpackage_preTest.tgz ../test
-# rm -rf DeploymentWebApp
+mv webpackage_preTest.tgz ../test
+rm -rf DeploymentWebApp
 
-# # untar
-# cd ../test
-# tar -zxvf webpackage_preTest.tgz
+# untar
+cd ../test
+tar -zxvf webpackage_preTest.tgz
 
-# # perform test/manipulation functions
+# perform test/manipulation
+# check static content is properly constructed
+tidy /var/www/*
 
-# # tar it back up
-# tar -zcvf webpackage_preDeploy.tgz DeploymentWebApp
+# test dynamic content functions as required
+# ie perl script enters data into database
+# configure mysql
+echo "Testing if data added to mysql"
+cat <<FINISH | mysql -uroot -ppassword
+drop database if exists dbtest;
+CREATE DATABASE dbtest;
+GRANT ALL PRIVILEGES ON dbtest.* TO dbtestuser@localhost IDENTIFIED BY 'dbpassword';
+use dbtest;
+drop table if exists custdetails;
+create table if not exists custdetails ( name VARCHAR(30) NOT NULL DEFAULT '', address VARCHAR(30) NOT NULL DEFAULT '' );
+insert into custdetails (name,address) values ('Niall Ryan', 'Rathmines');
+select * from custdetails;
+FINISH
+echo "Data added... Look at the line above"
 
-# # set ERRORCHECK if any errors
+# add more tests here
 
-# # check that ERRORCHECK is not 0
-# if [ $ERRORCHECK -eq 0 ]
-# then
-# 	# move webpackage + deployment script to AWS server
+# tar package back up
+tar -zcvf webpackage_preDeploy.tgz DeploymentWebApp
 
-# 	# untar file
+# check that ERRORCHECK is not 0
+if [ $ERRORCHECK -eq 0 ]
+then
+	# backup content
+	# mysqldump > db_backup
+	# scp db_backup testuser@whatever_BID_server_ip_is
 
-# 	# perform deployment
+	# move webpackage + monitoring script to deployment server
+	
+	scp -i ~/keypair1.pem webpackage_preDeploy.tgz ubuntu@ec2-54-194-154-110.eu-west-1.compute.amazonaws.com:~
+	#
+	# ssh into AWS instance
+	ssh -t -i ~/keypair1.pem ubuntu@ec2-54-194-154-110.eu-west-1.compute.amazonaws.com bash -c "'
+	#
+	# deploying to AWS server not possible due to errors installing Perl CGI handlers
+	# 
+	# deploying to different VirtualBox environment instead
+	
+	# copy webpackage over
+	# scp webpackage_preDeploy.tgz testuser@whateveripaddress:~
 
-# 	echo Doing Deployment
-# fi
+	# copy monitoring script over
+
+	# ssh into environment
+	# ssh testuser@whateveripaddress "'
+
+	# create sandbox
+	cd /tmp
+	mkdir $SANDBOX
+	cd $SANDBOX
+
+	# test resources
+	# check memory
+	# free, vmstat, top
+
+	# check disk
+	# df -h
+
+	# check network
+	# iostat, iotop, netstat
+
+	# clean environment ie un- and re-install apache and mysql
+	# stop services
+	service apache2 stop
+	service mysql stop
+
+	# uninstall
+	apt-get -q -y remove apache2
+	apt-get -q -y remove mysql-client mysql-server
+	echo mysql-server mysql-server/root_password password password | debconf-set-selections
+	echo mysql-server mysql-server/root_password_again password password Z debconf-set-selections
+
+	# update apt repo
+	apt-get update
+
+	# reinstall
+	apt-get -q -y install apache2
+	apt-get -q -y install mysql-client mysql-server
+
+	# untar app
+	tar -zcxf webpackage_preDeploy.tgz DeploymentWebApp
+
+	# move components to /www and /cgi-bin
+	cp DeploymentWebApp/www/* /var/www
+	cp DeploymentWebApp/cgi-bin/* /etc/cgi-bin/
+
+	# test necessary files are in place
+	DEPINDEX="/var/www/index.html"
+	DEPAFPL="/etc/cgi-bin/accept_form.pl"
+	DEPHW="/etc/cgi-bin/hello_world.pl"
+	DEPTDB="etc/cgi-bin/testdb.pl"
+	if [ -e "$DEPINDEX" ] && [ -e "$DEPAFPL" ] && [ -e "$DEPHW" ] && [ -e "$DEPTDB" ]
+	then
+		echo "All files in place"
+	else
+		echo "Files not in place, exiting..."
+		exit
+	fi
+
+	# configure crontab to run monitoring script
+	# modified from http://stackoverflow.com/questions/610839/how-can-i-programatically-create-a-new-cron-job
+	# configure crontab, make sure new cron job is unique
+	(crontab -l ; echo "* * * * * ~/logmon.sh") | uniq - | crontab - 
+
+	# end ssh commands
+	'"
+
+	echo "Deployment completed successfully."
+else
+	echo "Errors in Build, Integration or Test Phase... exiting..."
+	exit
+fi
